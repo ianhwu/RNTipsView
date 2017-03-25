86c8e31861d2033aff50711c2a5bda578661801d
yan <yan@aetheris.com.cn>
2017-03-25 18:40:57 +0800

    no message

---
+++ Example/tips/index.ios.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ImageStore
+} from 'react-native';
+import TipsView from 'react-native-tipsview';
+
+export default class tips extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageURI:'',
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TipsView ref='tipsView' style={styles.tipsViewStyle}>
+          {
+            this.state.imageURI.length > 0 ? (
+              <Image source={{uri:this.state.imageURI}} style={styles.previewStyle}/>
+            ) : null
+          }
+        </TipsView>
+        <TouchableOpacity onPress={()=>{
+          this.refs.tipsView.clear();
+          this.setState({imageURI:''});
+        }}>
+          <Text>{'clear'}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>{
+          this.refs.tipsView.export().then(result=>{
+            this.setState({imageURI:result});
+          }, error=>{
+            console.warn(error);
+          });
+        }}>
+          <Text>{'cut image'}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  tipsViewStyle: {
+    flex:1,
+    backgroundColor:'white',
+  },
+  previewStyle: {
+    position:'absolute',
+    width: 100,
+    height: 100,
+    resizeMode:'contain',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('tips', () => tips);
